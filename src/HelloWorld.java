f5616cf465f61b252b8911362bd678935f232b79
rtx64 <rtx64@DESKTOP-TK3A7MA>
2020-10-26 15:23:59 +0100

    Inicijalni commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,26 @@
+
+public class HelloWorld
+{
+	///
+	//
+	//
+	
+	static class Proba
+	{
+		public void Ajde()
+		{
+			System.out.println("Ajde method");
+		}
+	}
+	
+
+	public static void main(String[] args)
+	{
+		Proba p = new Proba();
+		p.Ajde();
+		
+		System.out.print("Neno");
+
+	}
+
+}
